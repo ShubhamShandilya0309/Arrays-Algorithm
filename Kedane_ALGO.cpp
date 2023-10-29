5782e88b821ae360ed44e88e42eed8898268ebe4
Shubham Shandilya <128954805+ShubhamShandilya0309@users.noreply.github.com>
2023-10-29 11:51:57 +0530

    Update Kedane_ALGO.cpp
    previously there is some mistakes as the inputs are in int format and not in long long and also maxi=max(sum,maxi) wasn't at its correct position.

--- Kedane_ALGO.cpp
+++ Kedane_ALGO.cpp
@@ -1,12 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 //Maximum Subarray Sum
-int kedaneAlgorithm(int arr[],int n){
-    int maxi=INT_MIN;
-    int sum=0;
+long long kedaneAlgorithm(int arr[],int n){
+    long long maxi=INT_MIN;
+    long long sum=0;
     for(int i=0;i<n;i++){
-        maxi=max(maxi,sum);
         sum+=arr[i];
+        maxi=max(maxi,sum);
         if(sum<0) sum=0;
     }
     return maxi;
